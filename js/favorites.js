81ec97ff13bfaeb4a3bf1e241df5559d6c8740d2
Diogo <2241572@my.ipleiria.pt>
2024-12-31 19:14:11 +0000

    add pagina fav + organização + ajustes

---
+++ js/favorites.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countryCard = document.getElementById('country-cards');
+    const searchBar = document.getElementById('search-bar');
+    const errorMSG = document.getElementById('error-msg');
+    const localStorage = window.localStorage;
+
+    const favoriteCountries = JSON.parse(localStorage.getItem('favoriteCountries')) || [];
+    let allCountries = [];
+
+    function loadCountries() {
+
+        const url = "https://restcountries.com/v3.1/all"
+
+        $.ajax({
+            url: url,
+            method: "GET",
+            success: function (countries) {
+                countries.sort((a, b) => (a.translations.por.common || a.name.common || '').localeCompare(b.translations.por.common || b.name.common || '', 'pt', { sensitivity: 'base' })); 
+                allCountries = countries;
+                showCountries(countries);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+    function showCountries(countries, isSearch = false) {
+        countryCard.innerHTML = '';
+        errorMSG.style.display = 'none';
+
+        // pras labels ficarem todas pipis
+        if (favoriteCountries.length === 0) {
+            errorMSG.textContent = 'Não possui nenhum país favorito.';
+            errorMSG.style.display = 'block';
+            return;
+        }
+        else if (isSearch){
+            errorMSG.textContent = 'Sem resultados...';
+            errorMSG.style.display = 'block';
+        }
+
+        countries.forEach((country) => {
+            errorMSG.style.display = 'none';
+
+            if (favoriteCountries.includes(country.cca3)) {
+                const cardDiv = document.createElement('div');
+                cardDiv.className = `col-md-3`;
+                
+                const isFavorite = favoriteCountries.includes(country.cca3);
+                const countryName = country.translations.por?.common || country.name.common;
+    
+                cardDiv.innerHTML = `
+                <div class="card my-3 flag-card">
+                    <img src="${country.flags.svg}" class="card-img-top rounded-0" alt="${countryName} Flag">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <p class="flag-caption mb-0">${countryName}</p>
+                        <i class="bi ml-auto star" style="font-size: 1.3rem; color: black;"></i>
+                     </div>
+                </div>`;
+
+                const starIcon = cardDiv.querySelector('.star');
+                starIcon.classList.add(isFavorite ? 'bi-star-fill' : 'bi-star');
+                starIcon.style.color = isFavorite ? '#FFD700' : 'black';
+
+                cardDiv.querySelector('.flag-card').addEventListener('click', (event) => {
+                    if (!event.target.classList.contains('star')) {
+                        window.location.href = `country-detail.html#${country.cca3}`;
+                    }
+                });
+
+                //toogle favoritos
+                starIcon.addEventListener('click', () => toogleFavorites(country, starIcon));
+
+                countryCard.appendChild(cardDiv);
+            }
+        });
+    }
+
+    function toogleFavorites(country, starIcon){
+        const isFavorite = favoriteCountries.includes(country.cca3);
+
+        if (isFavorite) {
+            favoriteCountries.splice(favoriteCountries.indexOf(country.cca3), 1);
+            starIcon.classList.replace('bi-star-fill', 'bi-star');
+            starIcon.style.color = 'black';
+        } else {
+            favoriteCountries.push(country.cca3);
+            starIcon.classList.replace('bi-star', 'bi-star-fill');
+            starIcon.style.color = '#FFD700';
+        }
+
+        localStorage.setItem('favoriteCountries', JSON.stringify(favoriteCountries));
+        showCountries(allCountries.filter(c => favoriteCountries.includes(c.cca3)));
+    }
+
+    searchBar.addEventListener('input', (event) => {
+        const searchQuery = event.target.value.toLowerCase();
+
+        const filteredCountries = allCountries.filter(country => 
+            favoriteCountries.includes(country.cca3) && 
+            (country.translations.por.common || country.name.common || '').toLowerCase().includes(searchQuery)
+        );
+
+        showCountries(filteredCountries, true);
+    });
+
+    // MAIN
+    loadCountries();
+});
--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const countryCard = document.getElementById('country-cards');
+    const localStorage = window.localStorage;
+    const favoriteCountries = JSON.parse(localStorage.getItem('favoriteCountries')) || [];
 
     function loadRandomCountries() {
 
@@ -21,34 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
     function showCountries(countries) {
         countryCard.innerHTML = '';
 
-        countries.forEach((country, index) => {
-
+        countries.forEach((country) => {
             const cardDiv = document.createElement('div');
-            cardDiv.className = `col-md-3 ${index === 0 ? 'selected' : ''}`;
-
+            cardDiv.className = `col-md-3`;
+            
+            const isFavorite = favoriteCountries.includes(country.cca3);
             const countryName = country.translations.por?.common || country.name.common;
-            const localStorage = window.localStorage;
 
             cardDiv.innerHTML = `
-                <div class="card flag-card ${index === 0 ? 'selected' : ''}">
-                    <img src="${country.flags.svg}" class="card-img-top rounded-0" alt="${countryName} Flag">
-                    <div class="card-body d-flex justify-content-between align-items-center">
-                        <p class="flag-caption">${countryName}</p>
-                        <i class="bi ml-auto star" style="font-size: 1.3rem; color: black;"></i>
-                     </div>
-                </div>`;
+            <div class="card flag-card">
+                <img src="${country.flags.svg}" class="card-img-top rounded-0" alt="${countryName} Flag">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <p class="flag-caption">${countryName}</p>
+                    <i class="bi ml-auto star" style="font-size: 1.3rem; color: black;"></i>
+                    </div>
+            </div>`;
 
-            if (typeof localStorage !== 'undefined') {
-                const favoriteCountries = JSON.parse(localStorage.getItem('favoriteCountries')) || [];
-
-                if (favoriteCountries.includes(country.cca3)) {
-                    cardDiv.querySelector('.star').classList.add('bi-star-fill');
-                    cardDiv.querySelector('.star').style.color = '#FFD700';
-                }
-                else {
-                    cardDiv.querySelector('.star').classList.add('bi-star');
-                }
-            }
+            const starIcon = cardDiv.querySelector('.star');
+            starIcon.classList.add(isFavorite ? 'bi-star-fill' : 'bi-star');
+            starIcon.style.color = isFavorite ? '#FFD700' : 'black';
 
             cardDiv.querySelector('.flag-card').addEventListener('click', (event) => {
                 if (!event.target.classList.contains('star')) {
@@ -56,38 +49,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
 
-            cardDiv.querySelector('.star').addEventListener('click', (event) => {
-                if (typeof localStorage !== 'undefined') {
-
-                    const favoriteCountries = JSON.parse(localStorage.getItem('favoriteCountries')) || [];
-
-                    if (event.target.classList.contains('bi-star')) {
-
-                        favoriteCountries.push(country.cca3);
-                        localStorage.setItem('favoriteCountries', JSON.stringify(favoriteCountries));
-
-                        event.target.classList.remove('bi-star');
-                        event.target.classList.add('bi-star-fill');
-                        event.target.style.color = '#FFD700';
-                    } else {
-                        const countryIndex = favoriteCountries.indexOf(country.cca3);
-                        console.log("ci:", countryIndex);
-
-                        if (countryIndex > -1) {
-                            favoriteCountries.splice(countryIndex, 1);
-                            console.log("fa: ", favoriteCountries);
-                            localStorage.setItem('favoriteCountries', JSON.stringify(favoriteCountries));
-                        }
-
-                        event.target.classList.remove('bi-star-fill');
-                        event.target.classList.add('bi-star');
-                        event.target.style.color = 'black';
-                    }
-                }
-            });
+            //toogle favoritos
+            starIcon.addEventListener('click', () => toogleFavorites(country, starIcon));
 
             countryCard.appendChild(cardDiv);
         });
+
+        function toogleFavorites(country, starIcon){
+            const isFavorite = favoriteCountries.includes(country.cca3);
+    
+            if (isFavorite) {
+                favoriteCountries.splice(favoriteCountries.indexOf(country.cca3), 1);
+                starIcon.classList.replace('bi-star-fill', 'bi-star');
+                starIcon.style.color = 'black';
+            } else {
+                favoriteCountries.push(country.cca3);
+                starIcon.classList.replace('bi-star', 'bi-star-fill');
+                starIcon.style.color = '#FFD700';
+            }
+    
+            localStorage.setItem('favoriteCountries', JSON.stringify(favoriteCountries));
+        }
     }
 
     // MAIN
